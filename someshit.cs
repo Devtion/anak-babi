08d667507f399732414584cbeac2b30d8445e68f
Skofos <root@skofos.xyz>
2020-07-16 12:49:08 +0700

    Update someshit.cs

--- someshit.cs
+++ someshit.cs
@@ -1,1 +1,9 @@
+using System;
 
+public class fuck 
+{
+  int lmao = 1337;
+  float kek = 800.20;
+  double microsoft = 1337.200;
+  string dllimportsuck = "bisanya cuma loop, var, shit kek lmao";
+}
